4a1f6e8b3ec60b753c9005e3a66f14359408e172
Ruhan <ruhancvbello@gmail.com>
2019-01-17 00:33:55 -0200

    Aula 3: andando

---
+++ Assets/Scripts/PlatformerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlatformerController : MonoBehaviour {
+    public float speed = 10;
+
+    private Rigidbody myRigidbody;
+
+    // Use this for initialization
+    void Start() {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        myRigidbody.velocity = new Vector3(horizontal * speed, myRigidbody.velocity.y, 0);
+
+        if (horizontal > 0)
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+
+        if (horizontal < 0)
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+    }
+}

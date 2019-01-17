71bf6a9013989bbfee6607c65bb984e447a7b39c
Ruhan <ruhancvbello@gmail.com>
2019-01-17 01:42:29 -0200

    Aula 10: inimigo e spawn de prefabs

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+    public GameObject metalBallPrefab;
+
+    public float intervalBetweenSpawns;
+
+    private float spawnTimer;
+
+    private void Start() {
+        spawnTimer = intervalBetweenSpawns;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        spawnTimer -= Time.deltaTime;
+
+        if (spawnTimer <= 0) {
+            spawnTimer = intervalBetweenSpawns;
+            Instantiate(metalBallPrefab, transform.position + Vector3.up, metalBallPrefab.transform.rotation);
+        }
+    }
+}

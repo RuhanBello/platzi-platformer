e9b3a1b290920873999ce71da755c4ca59e78000
Ruhan <ruhancvbello@gmail.com>
2019-01-17 01:01:31 -0200

    Aula 7: iluminacao e tipos de luzes

--- Assets/Scripts/PlatformerController.cs
+++ Assets/Scripts/PlatformerController.cs
@@ -10,10 +10,12 @@ public class PlatformerController : MonoBehaviour {
     public GroundDetector groundDetector;
 
     private Rigidbody myRigidbody;
+    private Animator animator;
 
     // Use this for initialization
     void Start() {
         myRigidbody = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -23,14 +25,17 @@ public class PlatformerController : MonoBehaviour {
         if (groundDetector.isGrounded)
             myRigidbody.velocity = new Vector3(horizontal * speed, myRigidbody.velocity.y, 0);
 
+        GetComponentInChildren<Animator>().SetFloat("Speed", Mathf.Abs(horizontal));
+
         if (horizontal > 0)
             transform.rotation = Quaternion.Euler(0, 90, 0);
-
+        
         if (horizontal < 0)
             transform.rotation = Quaternion.Euler(0, -90, 0);
 
         if (groundDetector.isGrounded && Input.GetKeyDown(KeyCode.Space)) {
             myRigidbody.AddForce(Vector3.up * jumpForce);
+            GetComponentInChildren<Animator>().SetTrigger("Jump");
         }
     }
 }
